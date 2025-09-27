f672e469e929c8a6db7b5cba5b0766e99dc7f8e6
Jakobo Alvarez <estith.alvarez64302@ucaldas.edu.co>
2025-09-27 17:24:41 -0500

    Agregando clase padre *Shape*

--- Shape.java
+++ Shape.java
@@ -1,4 +1,37 @@
-public class Shape{
-    private Shape shape = new Shape();
-    ///jkshfgsjdgfjdsfjsdj
+public abstract class Shape{
+    //Variables
+    private int id;
+    private double xCoordinate;
+    private double yCoordinate;
+    //Constructor
+    public Shape(int id, double xCoordinate, double yCoordinate){
+        setId(id);
+        setXCoordinate(xCoordinate);
+        setYCoordinate(yCoordinate);
+    }
+    //Setters
+    public void setId(int id){
+        if( id > 0){
+            this.id = id;
+        }
+    }
+    public void setXCoordinate(double xCoordinate){
+        this.xCoordinate = xCoordinate;
+    }
+    public void setYCoordinate(double yCoordinate){
+        this.yCoordinate = yCoordinate;
+    }
+    //getters
+    public int getId(){
+        return id;
+    }
+    public double getXCoordinate(){
+        return xCoordinate;
+    }
+    public double getYCoordinate(){
+        return yCoordinate;
+    }
+    //getters abstractos
+    public abstract double getArea();
+    public abstract double getPerimeter(); 
 }
