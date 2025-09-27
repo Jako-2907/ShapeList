7dbbb244ef25c7a1e87c7f315684f9bc2b29357a
Jakobo Alvarez <estith.alvarez64302@ucaldas.edu.co>
2025-09-27 18:51:48 -0500

    Agregando *LisShape*

--- ListShape.java
+++ ListShape.java
@@ -2,6 +2,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListShape {
-    private List<Shape> list = new ArrayList();
-    
+    //Variable List
+    private List<Shape> shapes;
+    //Constructors
+    public ListShape(){
+        shapes = new ArrayList<>();
+    }
+    public ListShape(List<Shape> shapes){
+        setShapes(shapes);
+    }
+    //getters
+    public List<Shape> getShapes(){
+        return shapes;
+    }
+    //setters
+    public void setShapes(List<Shape> shapes){
+        if(shapes != null){
+            this.shapes = shapes;
+        }else{
+            this.shapes = new ArrayList<>();
+        }
+    }
+    /*Methods
+    Añadir una figura a la lista
+    Consultar y mostrar la información de una figura específica a partir de su id.
+    Calcular y mostrar el área total y el perímetro total de todas las figuras almacenadas en la lista.
+    Salir del programa.
+    */
+    public void addShape(Shape shapes){
+        if(shapes != null){
+            this.shapes.add(shapes);
+        }
+    }
+    public Shape getShapeById(int id){
+        for(Shape shape:shapes){
+            if(shape.getId() == id){
+                return shape;
+            }
+        }
+        return null;
+    }
+    public double getTotalArea(){
+        double total = 0;
+        for(Shape shape : shapes){
+            total += shape.getArea();
+        }
+        return total;
+
+    }
+    public double getTotalPerimeter(){
+        double total = 0;
+        for(Shape shape:shapes){
+            total += shape.getPerimeter();
+        }
+        return total;
+    }
 }
+    
+
