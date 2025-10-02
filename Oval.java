7930174411309e81e81cfc050a66d95622757d21
Jakobo Alvarez <estith.alvarez64302@ucaldas.edu.co>
2025-10-02 14:58:53 -0500

    Agregando Oval

--- Oval.java
+++ Oval.java
@@ -1,32 +1,34 @@
-public class Oval extends Shape{
+public class Oval extends Circle{
     //variables
-    private double radio;
     private double radio2;
 
     //constructor
     public Oval(int id, double xCoordinate, double yCoordinate, double radio, double radio2){
-        super(id,xCoordinate,yCoordinate);
-        this.radio = radio;
+        super(id,xCoordinate,yCoordinate,radio);
         this.radio2 = radio2;
     }
     //setters
-    public void setArea(){
-
-    }
-    public void setPerimeter(){
+    public void setRadio2(double radio2){
+        if (radio2 < 0){
+            this.radio2 = 0;
+        }else{
+            this.radio2 = radio2;
+        }
 
     }
     //getters
+    public double getRadio2(){
+        return radio2;
+    }
+    //getters abstractos
     @Override
     public double getArea(){
-        return 0;
+        return (Math.PI * getRadio2()* getRadio());
 
     }
     @Override
     public double getPerimeter(){
-        return 0;
-
+        return (Math.PI*(3*(getRadio() + getRadio2())-Math.sqrt((3*getRadio()+getRadio2())*(getRadio()+3*getRadio2()))));
+        //π [ 3(a + b) - √((3a + b)(a + 3b)) ]
     }
-
-
 }
